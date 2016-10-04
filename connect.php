6ae7c92fbd135d05a77ca29d036530fe4ae484f4
BasileDubruque <basile.dubruque@epsi.fr>
2016-10-04 16:52:57 +0200

    Add connect.php

---
+++ connect.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Yardim - Accueil</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="page-header">
+    <div class="">
+        <h1><a href="index.php" class="">Yardim</a></h1>
+    </div>
+</div>
+<div class="">
+    <p>Se connecter</p>
+    <div>
+        <form action="#" method="post">
+            <p><label for='Mail'>Mail :</label> <input class="" type="email" name="Mail" placeholder="mail@example.com"/></p>
+            <p><label for="Password">Mot de passe :</label> <input class="" type="password" name="Password" placeholder="Password"/></p>
+            <input type="submit" value="Connexion"/>
+        </form>
+        <button href="#">Pas encore de compte</button>
+
+    </div>
+</div>
+</body>
+</html>
