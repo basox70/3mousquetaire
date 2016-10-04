e346757febbf2268993729a360a7e84c5d8ca92e
BasileDubruque <basile.dubruque@epsi.fr>
2016-10-04 14:56:35 +0200

    Add bootstrap and page title

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<h1>It works!!!</h1>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Yardim - Accueil</title>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="">
+            <div class="">
+                <h1><a href="index.php" alt="Yardim" class="">Yardim</a></h1>
+            </div>
+        </div>
+
+    </body>
+</html>
