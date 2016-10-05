c5d8bd8e3a95f76dc6a3cd42d7b7018fb977cf54
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 09:58:22 +0200

    Update Requests.php 8

--- requests.php
+++ requests.php
@@ -23,13 +23,19 @@ include ('ConfigDB.php');
     echo $_GET['id']; // Récupération de l'ID dans l'URL
     $reponse = $bdd->query('SELECT * FROM Requests WHERE Id ='.$_GET['id']);
 
-    echo '<div class = "container">';
+    echo '<div class = "container" style="border: 1px solid;">';
 
     while ($donnees = $reponse->fetch()) {
         echo '<div class="row">';
-        echo '<div class="col-sm-1">test</div>';
-        echo '<div class="col-sm-3">Titre</div>';
-        echo '<div class="col-sm-6">'.$donnees['Title'].'</div>';
+            echo '<div class="col-sm-1">test</div>';
+            echo '<div class="col-sm-3">Titre</div>';
+            echo '<div class="col-sm-6">'.$donnees['Title'].'</div>';
+        echo '</div>';
+
+        echo '<div class="row">';
+            echo '<div class="col-sm-1">test</div>';
+            echo '<div class="col-sm-3">Description</div>';
+            echo '<div class="col-sm-6">'.$donnees['Description'].'</div>';
         echo '</div>';
     }
 
