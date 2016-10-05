b6cfafd9c1b10515347199e27337fea4a398bf92
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 13:56:39 +0200

    add addRequest.php 2

--- addRequest.php
+++ addRequest.php
@@ -25,9 +25,9 @@ include "ConfigDB.php";
         Proposer une nouvelle annonce ..
     <form action="registerConfirmation.php" method="post">
         Titre : <input type="text" name="title" value=""><br />
-        Description : <input type="text" name="description" value=""><br />
+        Description : <input type="text" size="200" name="description" value=""><br />
         Email : <input type="email" name="email" value=""><br />
-        Numéro de téléphone : * <input type="texts" name="number" value=""><br />
+        Numéro de téléphone : * <input type="text" size="10" name="number" value=""><br />
         Votre nom :  <input type = "text" name = "name" value = ""><br />
         Ville : <input type = "text" name = "city" value = ""><br />
         Département : <select name="department">
