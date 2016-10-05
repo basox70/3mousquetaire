d8ba787bc965ab0dcd1fbb2a48f2568527953867
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 11:01:10 +0200

    add registerConfirmation.php

--- registerConfirmation.php
+++ registerConfirmation.php
@@ -1,9 +1,21 @@
 <?php
 
+
+
+
 $prenom = $_POST['name'];
 $nom = $_POST['surname'];
+$mail = $_POST['email'];
+$birthDate = $_POST['birthDate'];
+$city = $_POST['city'];
+$department = $_POST['department'];
+$pass = $_POST['pass'];
 
 
 echo $prenom;
-
 echo $nom;
+echo $mail;
+echo $birthDate;
+echo $city;
+echo $department;
+echo $pass;
