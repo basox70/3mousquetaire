703255d6f5c3612f8b4477579e389149d509f809
BasileDubruque <basox70@users.noreply.github.com>
2016-10-05 15:30:19 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,34 +1,37 @@
+<?php
+session_start();
+?>
 <html>
-<head>
-    <meta charset="utf-8"/>
-    <title>Yardim - Accueil</title>
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-</head>
-<body>
-<div class="page-header">
-    <div class="">
-        <h1><a href="index.php" class="">Yardim</a></h1>
-    </div>
-</div>
-<div class="">
-    <p>Se connecter</p>
-    <div>
-        <form action="#" method="post">
-            <div class="form-group row">
-                <label for='Mail'>Mail :</label>
-                <input class="form-control" type="email" id="Mail" placeholder="mail@example.com"/>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Yardim - Accueil</title>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    </head>
+    <body>
+        <div class="page-header">
+            <div class="">
+                <h1><a href="index.php" class="">Yardim</a></h1>
             </div>
-            <div class="form-group row">
-                <label for="Password">Mot de passe :</label>
-                <input class="form-control" type="password" id="Password" placeholder="Password"/>
+        </div>
+        <div class="">
+            <p>Se connecter</p>
+            <div class="col-lg-5">
+                <form action="function.php" method="post">
+                    <div class="form-group row">
+                        <label for='Mail'>Mail :</label>
+                        <input class="form-control" type="email" id="Mail" name="mail" placeholder="mail@example.com"/>
+                    </div>
+                    <div class="form-group row">
+                        <label for="Password">Mot de passe :</label>
+                        <input class="form-control" type="password" name="password" id="Password" placeholder="Password"/>
+                    </div>
+                    <input class="btn btn-primary" type="submit" value="Connexion"/>
+                </form>
+                <p>Pas encore de compte</p>
+                <button class="btn btn-primary" href="#">Inscrivez-vous!</button>
             </div>
-            <input type="submit" value="Connexion"/>
-        </form>
-
-        <button class="btn btn-primary" href="#">Pas encore de compte</button>
-
-    </div>
-</div>
-</body>
+        </div>
+    </body>
 </html>
