5984541f1f3e8669d9f73d07ddc86f9d817406d3
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 09:25:23 +0200

    Last requests & requests.php 9

--- requests.php
+++ requests.php
@@ -1,2 +1,21 @@
 <?php
-echo $_GET['id'];
+include ('ConfigDB.php');
+?>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Yardim - Accueil</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="page-header">
+    <div class="">
+        <h1><a href="index.php" alt="Yardim" class="">Yardim</a></h1>
+    </div>
+    <div>
+        Connexion
+    </div>
+</div>
+<?php
+    echo $_GET['id'];
