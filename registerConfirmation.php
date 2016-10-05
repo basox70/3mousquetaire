08867cb981e662f2689c967fa7aeedfeeede26f1
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 11:04:03 +0200

    add registerConfirmation.php 2

--- registerConfirmation.php
+++ registerConfirmation.php
@@ -1,6 +1,5 @@
 <?php
-
-
+include "ConfigDB.php";
 
 
 $prenom = $_POST['name'];
@@ -23,3 +22,18 @@ echo $pass;
 $pass_hache = sha1($_POST['pass']);
 
 echo $pass_hache;
+
+// Insertion
+$req = $bdd->prepare('INSERT INTO User(Name, Surname, Mail, BirthDate, City, Department, Password)
+                    VALUES(:name, :surname, :mail, :birthDate, :city, :department, :pass)');
+$req->execute(array(
+    'name' => $pseudo,
+    'surname' => $pass_hache,
+    'mail' => $birthDate,
+    'birthDate' => $email,
+    'city' => $city,
+    'department' => $department,
+    'pass' => $pass_hache));
+if (isset($erreur)) echo '<br />',$erreur;
+
+echo"ok";
