09d64d82c009266a9287c77233d86bea6e6a9397
BasileDubruque <basox70@users.noreply.github.com>
2016-10-05 15:30:42 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,23 +1,44 @@
-<html>
 <?php
-include ('ConfigDB.php');
-
-function Connexion($mail, $password)
+require('ConfigDB.php');
+session_start();
+if(isset($_POST['mail']))
 {
-    $respond = $bdd->query('SELECT * FROM User where mail = $mail');
-    while($donnees = $respond->fetch())
-    {
-        if($password == respond['password'])
-        {
-            $_SESSION['name'] = $donnees['name'];
-        }
-
-    }
-
-
+    Connection($_POST['mail'],$_POST['password'],$bdd);
 }
 
+if($_GET['deconnect'])
+{
+    session_destroy();
+    header("Location: index.php");
+}
+
+function Connection($mail, $password,$bdd)
+{
+    $request = "SELECT * FROM User where mail = \"$mail\"";
+    $respond = $bdd->query($request);
+    $donnees = $respond->fetch();
+    $connected = 1;
+    while($donnees && $connected != 2)
+    {
+        $password= sha1($password);
+        if($password == $donnees['Password'])
+        {
+            $_SESSION['datas'] = $donnees;
+            var_dump($_SESSION);
+            $connected = 0;
+            $_SESSION['connected'] = true;
+            echo "Bienvenue ".$_SESSION['datas']['Name'];
+            header("Location: index.php");
+        }
+        else
+        {
+            $connected = 2;
+            echo "Erreur de connexion";
+        }
+    }
+    if ($connected==1)
+    {
+        echo "Le compte n'existe pas";
+    }
+}
 ?>
-
-
-</html>
