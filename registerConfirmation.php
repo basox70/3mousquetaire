428da493bfc74838fe6a46e820d02bf868dbc98b
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 11:09:27 +0200

    add registerConfirmation.php 5

--- registerConfirmation.php
+++ registerConfirmation.php
@@ -1,5 +1,27 @@
 <?php
 include "ConfigDB.php";
+?>
+
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Yardim - Accueil</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+<div class="page-header">
+    <div class="">
+        <h1><a href="index.php" alt="Yardim" class="">Yardim</a></h1>
+    </div>
+    <div>
+        Connexion
+    </div>
+    <div><a href="register.php">Inscription</a></div>
+</div>
+<div>
+    
+    <?php
 
 
 $prenom = $_POST['name'];
@@ -13,7 +35,6 @@ $pass = $_POST['pass'];
 
 $pass_hache = sha1($_POST['pass']);
 
-echo $pass_hache;
 
 // Insertion
 $req = $bdd->prepare('INSERT INTO User(Name, Surname, Mail, BirthDate, City, Department, Password)
