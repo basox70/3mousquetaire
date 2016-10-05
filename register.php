38920205da368144811eee8e9a8d1a5d4d6e4584
Jules Marquis <JulesMarquis@MacBook-Air-de-Jules.local>
2016-10-05 10:51:51 +0200

    add register.php 2

--- register.php
+++ register.php
@@ -22,12 +22,30 @@
 
         Inscription à l'espace membre :<br />
         <form action="inscription.php" method="post">
-            Login : <input type="text" name="login" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>"><br />
-            Mot de passe : <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
-            Confirmation du mot de passe : <input type="password" name="pass_confirm" value="<?php if (isset($_POST['pass_confirm'])) echo htmlentities(trim($_POST['pass_confirm'])); ?>"><br />
+            Prénom : <input type="text" name="name" value=""><br />
+            Nom : <input type="text" name="surname" value=""><br />
+            Mail : <input type="email" name="email" value=""><br />
+            Date de Naissance : <input type = "date" name = "birthDate" value = ""><br />
+            Ville : <input type = "text" name = "city" value = ""><br />
+            Département : <input type = "text" name = "department" value = ""><br />
+            Mot de passe : <input type="password" name="pass" value=""><br />
+            Confirmation du mot de passe : <input type="password" name="pass_confirm" value=""><br />
             <input type="submit" name="inscription" value="Inscription">
         </form>
 <?php
+$pass_hache = sha1($_POST['pass']);
+
+// Insertion
+$req = $bdd->prepare('INSERT INTO User(Name, Surname, Mail, BirthDate, City, Department, Password)
+                    VALUES(:name, :surname, :mail, :birthDate, :city, :department, :pass)');
+$req->execute(array(
+    'name' => $pseudo,
+    'surname' => $pass_hache,
+    'mail' => $birthDate,
+    'birthDate' => $email,
+    'city' => $city,
+    'department' => $department,
+    'pass' => $pass_hache));
 if (isset($erreur)) echo '<br />',$erreur;
 ?>
 </body>
