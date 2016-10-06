6f88750eed841aa6a31a9579093caa7e8d38681b
pizart <paul.izart@epsi.fr>
2016-10-06 13:49:18 +0200

    update register.php design

--- register.php
+++ register.php
@@ -11,33 +11,74 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 </head>
 <body>
-    <div class="page-header">
-        <div class="pull-left hidden-xs col-xs-4">&nbsp;</div>
-        <div class="pull-left hidden-xs image"><img src="img/logo-simple.png"></div>
-        <div class="banner">
-            <h1><a href="index.php" class="">Yardim</a></h1>
-        </div>
-        <div class="description pull-left col-xs-7 hidden-xs">
-            <h4><em>Le site d'aide a la personne pour les etudiants</em></h4>
-        </div>
+<div class="page-header">
+    <div class="pull-left hidden-xs col-xs-4">&nbsp;</div>
+    <div class="pull-left hidden-xs image"><img src="img/logo-simple.png"></div>
+    <div class="banner">
+        <h1><a href="index.php" class="">Yardim</a></h1>
     </div>
-    <div><a href="register.php">Inscription</a></div>
-    <div class="registration">
-        <p>Inscription à l'espace membre :</p>
-        <form action="registerConfirmation.php" method="post">
-            Prénom : <input type="text" name="name" value=""><br />
-            Nom : <input type="text" name="surname" value=""><br />
-            Mail : <input type="email" name="email" value=""><br />
-            Date de Naissance : <input type = "date" name = "birthDate" value = ""><br />
-            Ville : <input type = "text" name = "city" value = ""><br />
-            Département : <select name="department">
-                <option value="">Select...</option>
-                <?php echo $departments; ?>
-                            </select> <br />
+    <div class="description pull-left col-xs-7 hidden-xs">
+        <h4><em>Le site d'aide a la personne pour les etudiants</em></h4>
+    </div>
+</div>
+<div class="registration">
+    <div class="col-xs-3">&nbsp;</div>
+    <div class="col-lg-5">
+        <p class="form-group">Inscription à l'espace membre :</p>
 
-            Mot de passe : <input type="password" name="pass" value=""><br />
-            Confirmation du mot de passe : <input type="password" name="pass_confirm" value=""><br />
-            <input type="submit" name="inscription" value="Inscription">
+        <form action="registerConfirmation.php" method="post">
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='Prenom'>Prénom : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type="text" name="name" value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='Nom'>Nom : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type="text" name="surname" value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='Mail'>Mail : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type="email" name="email" value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='DateDeNaissance'>Date de naissance : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type = "date" name = "birthDate" value = "">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='city'>Ville : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type = "text" name = "city" value = "">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='departement'>Département : </label>
+                <div class="col-xs-9">
+                    <select class="form-control"name="department">
+                        <option value="">Selection départment...</option>
+                        <?php echo $departments; ?>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='password'>Mot de passe : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type="password" name="pass" value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-xs-3 col-form-label" for='pass_confirm'>Confirmation du mot de passe : </label>
+                <div class="col-xs-9">
+                    <input class="form-control" type="password" name="pass_confirm" value="">
+                </div>
+            </div>
+            <input class="btn btn-primary"type="submit" name="inscription" value="Inscription">
         </form>
     </div>
 </body>
