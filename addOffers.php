d15203fa94d4b34b3b878007240a64d3ef481fb7
pizart <paul.izart@epsi.fr>
2016-10-06 16:54:05 +0200

    update addOffers.php design

--- addOffers.php
+++ addOffers.php
@@ -4,11 +4,11 @@ session_start();
 ?>
 <html>
 <head>
-    <meta charset="utf-8"/>
-    <title>Yardim - Accueil</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta charset="utf-8"/>
+        <title>Yardim - Accueil</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
 <div class="page-header">
@@ -32,13 +32,7 @@ session_start();
                 { echo " <span class=\"pull-right\"><a class=\"btn btn-primary\" href='connect.php'>Se connecter</a>&nbsp;<a class=\"btn btn-primary\" href=\"register.php\">S'inscrire</a></span>"; }
                 else
                 {
-                        if($_SESSION['datas']['Nickname']== null)
-                        {
-                                echo "<span class=\"pull-right\" >Bienvenue " . $_SESSION['datas']['Name'] . "&nbsp;";
-                        }else
-                        {
-                                echo "<span class=\"pull-right\" >Bienvenue " . $_SESSION['datas']['Nickname'] . "&nbsp;";
-                        }
+                        echo "<span class=\"pull-right\" >Bienvenue ".$_SESSION['datas']['Name']."&nbsp;";
                         if(!$_SESSION['datas']['Admin']=='1')
                         {
                                 echo " <a class=\"btn btn-primary\" href=\"profile.php\" >Profil</a>";
@@ -47,146 +41,109 @@ session_start();
                 } ?>
         </div>
 </div>
-    <div>
+<div>
         <?php
-            if(!$_SESSION)
-            {
+        if(!$_SESSION)
+        {
                 echo "Vous devez être connecté pour publier une offre.";
                 echo '<br />';
                 echo 'Connectez vous <a href ="connect.php">ici</a>';
-            }
-            else
-            {?>
-                Proposer votre offre ..
-            <form action="addOffersConfirmation.php" method="post">
-                    Titre : <input type="text" name="title" value=""><br />
-                    Description : <textarea type="text" size="200" style="display: block; width: 20%;" name="description" value=""></textarea><br />
-
-                    Catégorie : <select name = "categorie">
-                    <?php
-                    $categories = $bdd->query("SELECT * FROM Category");
-                    while ($categorie = $categories->fetch()) {
-                        echo "<option value=".$categorie['Id'].">".$categorie['Name']."</option>";
-                    }
-                    ?>
-
-                    </select><br />
-                    Disponible à partir du  : <input type="date" name="dateD" value=""><br />
-                    Jusqu'au : <input type="date" name="dateF" value=""><br />
-                    Votre Email : <input type="email" name="email" value=""><br />
-                    Votre Numéro de téléphone : * <input type="text" size="10" name="number" value=""><br />
-                    Votre nom :  <input type = "text" name = "name" value = ""><br />
-                    Ville : <input type = "text" name = "city" value = ""><br />
-                    Département : <select name="department">
-                    <option value="">Select...</option>
-                    <option value="Ain">Ain</option>
-                    <option value="Aisne">Aisne</option>
-                    <option value="Allier">Allier</option>
-                    <option value="Alpes-de-Haute-Provence">Alpes-de-Haute-Provence</option>
-                    <option value="Hautes-Alpes">Hautes-Alpes</option>
-                    <option value="Alpes Maritimes">Alpes Maritimes</option>
-                    <option value="Ardèche">Ardèche</option>
-                    <option value="Ardennes">Ardennes</option>
-                    <option value="Ariège">Ariège</option>
-                    <option value="Aube">Aube</option>
-                    <option value="Aude">Aude</option>
-                    <option value="Aveyron">Aveyron</option>
-                    <option value="Bouches-du-Rhône">Bouches-du-Rhône</option>
-                    <option value="Calvados">Calvados</option>
-                    <option value="Cantal">Cantal</option>
-                    <option value="Charente">Charente</option>
-                    <option value="Charente-Maritime">Charente-Maritime</option>
-                    <option value="Cher">Cher</option>
-                    <option value="Corrèze">Corrèze</option>
-                    <option value="Corse Du Sud">Corse Du Sud</option>
-                    <option value="Haute Corse">Haute Corse</option>
-                    <option value="Côte-d'Or">Côte-d'Or</option>
-                    <option value="Côtes d'Armor">Côtes d'Armor</option>
-                    <option value="Creuse">Creuse</option>
-                    <option value="Dordogne">Dordogne</option>
-                    <option value="Doubs">Doubs</option>
-                    <option value="Drôme">Drôme</option>
-                    <option value="Eure">Eure</option>
-                    <option value="Eure-et-Loir">Eure-et-Loir</option>
-                    <option value="Finistère">Finistère</option>
-                    <option value="Gard">Gard</option>
-                    <option value="Haute Garonne">Haute Garonne</option>
-                    <option value="Gers">Gers</option>
-                    <option value="Gironde">Gironde</option>
-                    <option value="Hérault">Hérault</option>
-                    <option value="Ille-et-Vilaine">Ille-et-Vilaine</option>
-                    <option value="Indre">Indre</option>
-                    <option value="Indre-et-Loire">Indre-et-Loire</option>
-                    <option value="Isère">Isère</option>
-                    <option value="Jura">Jura</option>
-                    <option value="Landes">Landes</option>
-                    <option value="Loir-et-Cher">Loir-et-Cher</option>
-                    <option value="Loire">Loire</option>
-                    <option value="Haute Loire">Haute Loire</option>
-                    <option value="Loire Atlantique">Loire Atlantique</option>
-                    <option value="Loiret">Loiret</option>
-                    <option value="Lot">Lot</option>
-                    <option value="Lot-et-Garonne">Lot-et-Garonne</option>
-                    <option value="Lozère">Lozère</option>
-                    <option value="Maine-et-Loire">Maine-et-Loire</option>
-                    <option value="Manche">Manche</option>
-                    <option value="Marne">Marne</option>
-                    <option value="Haute Marne">Haute Marne</option>
-                    <option value="Mayenne">Mayenne</option>
-                    <option value="Meurthe-et-Moselle">Meurthe-et-Moselle</option>
-                    <option value="Meuse">Meuse</option>
-                    <option value="Morbihan">Morbihan</option>
-                    <option value="Moselle">Moselle</option>
-                    <option value="Nièvre">Nièvre</option>
-                    <option value="Nord">Nord</option>
-                    <option value="Oise">Oise</option>
-                    <option value="Orne">Orne</option>
-                    <option value="Pas-de-Calais">Pas-de-Calais</option>
-                    <option value="Puy-de-Dôme">Puy-de-Dôme</option>
-                    <option value="Pyrénées Atlantiques">Pyrénées Atlantiques</option>
-                    <option value="Hautes Pyrénées">Hautes Pyrénées</option>
-                    <option value="Pyrénées Atlantiques">Pyrénées Atlantiques</option>
-                    <option value="Bas-Rhin">Bas-Rhin</option>
-                    <option value="Haut-Rhin">Haut-Rhin</option>
-                    <option value="Rhône">Rhône</option>
-                    <option value="Haute Saône">Haute Saône</option>
-                    <option value="Saône-et-Loire">Saône-et-Loire</option>
-                    <option value="Sarthe">Sarthe</option>
-                    <option value="Savoie">Savoie</option>
-                    <option value="Haute Savoie">Haute Savoie</option>
-                    <option value="Paris">Paris</option>
-                    <option value="Seine Maritime">Seine Maritime</option>
-                    <option value="Seine-et-Marne">Seine-et-Marne</option>
-                    <option value="Yvelines">Yvelines</option>
-                    <option value="Deux-Sèvres">Deux-Sèvres</option>
-                    <option value="Somme">Somme</option>
-                    <option value="Tarn">Tarn</option>
-                    <option value="Tarn-et-Garonne">Tarn-et-Garonne</option>
-                    <option value="Var">Var</option>
-                    <option value="Vaucluse">Vaucluse</option>
-                    <option value="Vendée">Vendée</option>
-                    <option value="Vienne">Vienne</option>
-                    <option value="Haute Vienne">Haute Vienne</option>
-                    <option value="Vosges">Vosges</option>
-                    <option value="Yonne">Yonne</option>
-                    <option value="Territoire de Belfort">Territoire de Belfort</option>
-                    <option value="Essonne">Essonne</option>
-                    <option value="Hauts-de-Seine">Hauts-de-Seine</option>
-                    <option value="Seine-St-Denis">Seine-St-Denis</option>
-                    <option value="Val-de-Marne">Val-de-Marne</option>
-                    <option value="Val-D'Oise">Val-D'Oise</option>
-                    <option value="Guadeloupe">Guadeloupe</option>
-                    <option value="Martinique">Martinique</option>
-                    <option value="Guyane">Guyane</option>
-                    <option value="La Réunion">La Réunion</option>
-                    <option value="Mayotte">Mayotte</option>
-                </select> <br />
-                <input type="submit" name="addOffers" value="Publier votre Offre">
-                </form>
-<?php
         }
+        else
+        {?>
+        <div class="col-xs-3">&nbsp;</div>
+        <div class="col-lg-5">
+                <p class="form-group">Proposer votre offre ..</p>
+                <form action="addOffersConfirmation.php" method="post">
 
-        ?>
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='Titre'>Titre : </label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type="text" name="title" value=""><br />
+                                </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='Description'>Description : </label>
+                                <div class="col-xs-9">
+                                        <textarea class="form-control" type="text" rows="4" cols="600" style="display: block name="description" value=""></textarea><br />
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='Categorie'>Catégorie : </label>
+                                <div class="col-xs-9">
+                                        <select class="form-control" name = "categorie">
+
+                                                <?php
+                                                $categories = $bdd->query("SELECT * FROM Category");
+                                                while ($categorie = $categories->fetch()) {
+                                                        echo "<option value=".$categorie['Id'].">".$categorie['Name']."</option>";
+                                                }
+                                                ?>
+
+                                        </select><br />
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='from'>Disponible à partir de </label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type="date" name="dateD" value=""><br />
+                                </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='until'>Jusqu'au </label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type="date" name="dateF" value=""><br />
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='mail'>Votre E-mail </label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type="email" name="email" value=""><br />
+                                </div>
+                        </div>
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='Tel'>Votre numéro de téléphone *</label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type="text" size="10" name="number" value=""><br />
+                                </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='Nom'>Votre Nom</label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type = "text" name = "name" value = ""><br />
+                                </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='city'>Ville</label>
+                                <div class="col-xs-9">
+                                        <input class="form-control" type = "text" name = "city" value = ""><br />
+                                </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <label class="col-xs-3 col-form-label" for='department'>Département</label>
+                                <div class="col-xs-9">
+                                        <select class="form-control" name="department">
+                                                <option value="">Selection...</option>
+                                                <?php
+                                                echo $departments;
+                                                ?>
+                                        </select> <br />
+                                </div>
+                        </div>
 
 
-    </div>
+                        <input class="btn btn-primary" type="submit" name="addOffers" value="Publier votre Offre">
+
+                </form>
+                <?php
+                }
+
+                ?>
+
+
+        </div>
