85dd262f18a63745e085042648a3044a214ea800
basox70 <basile.dubruque@epsi.fr>
2016-10-06 16:55:06 +0200

    update admin page

--- admin.php
+++ admin.php
@@ -14,12 +14,6 @@ session_start();
     <body>
         <div class="page-header">
             <div class="pull-left hidden-xs col-xs-4">&nbsp;</div>
-            <?php
-            if($_SESSION && $_SESSION['datas']['Admin']=='1')
-            {
-                echo "<a class=\"admin\" href\"admin.php\">Administration</a>";
-            }
-            ?>
             <div class="pull-left hidden-xs image"><img src="img/logo-simple.png"></div>
             <div class="banner">
                 <h1><a href="index.php" class="">Yardim</a></h1>
@@ -44,6 +38,10 @@ session_start();
                     {
                         echo " <a class=\"btn btn-primary\" href=\"profile.php\" >Profil</a>";
                     }
+                    if($_SESSION['datas']['Admin']=='1')
+                    {
+                        echo "<a class=\"btn btn-danger\" href=\"admin.php\">Administration</a>";
+                    }
                     echo " <a class=\"btn btn-warning\" href='function.php?deconnect=true' >Deconnexion</a></span>";
                 } ?>
             </div>
@@ -58,6 +56,52 @@ session_start();
                 }
                 else
                 {
+                    //USERS
+                    $users = $bdd->query('SELECT Id, Name, Surname FROM User');
+
+                    echo "<div class=\"hidden-xs col-xs-3\">&nbsp;</div>";
+                    echo "<div class='col-lg-6'>";
+                    echo "<form class=\"form-group\" method='post' action='function.php'>";
+                    echo "<p>Utilisateurs inscrits :</p>";
+                    echo "<input type=\"hidden\" value=\"true\" name=\"deleteUser\" />";
+                    echo "<select class=\"form-control\" name=\"user\">";
+                    while($user = $users->fetch())
+                    {
+                        echo "<option value=\"".$user['Id']."\">".$user['Name']." ".$user['Surname']."</option>";
+                    }
+                    echo "</select><br/>";
+                    echo "<input class=\"btn btn-warning\" type=\"submit\" value=\"Supprimer l'(les) utilisateur(s)\"/>";
+                    echo "</form>";
+
+                    //DEMANDES
+                    $requests = $bdd->query('SELECT Id, Title, Mail FROM Requests');
+
+                    echo "<form class=\"form-group\" method='post' action='function.php'>";
+                    echo "<p>Demandes effectuées :</p>";
+                    echo "<input type=\"hidden\" value=\"true\" name=\"deleteRequest\" />";
+                    echo "<select class=\"form-control\" name=\"request\">";
+                    while($request = $requests->fetch())
+                    {
+                        echo "<option value=\"".$request['Id']."\">\"".$request['Title']."\" posté par : ".$request['Mail']."</option>";
+                    }
+                    echo "</select><br/>";
+                    echo "<input class=\"btn btn-warning\" type=\"submit\" value=\"Supprimer la(les) demande(s)\"/>";
+                    echo "</form>";
+
+                    //OFFRES
+                    $requests = $bdd->query('SELECT Id, Title FROM Offers');
+
+                    echo "<form class=\"form-group\" method='post' action='function.php'>";
+                    echo "<p>Demandes effectuées :</p>";
+                    echo "<input type=\"hidden\" value=\"true\" name=\"deleteOffer\" />";
+                    echo "<select class=\"form-control\" name=\"offer\">";
+                    while($request = $requests->fetch())
+                    {
+                        echo "<option value=\"".$request['Id']."\">".$request['Title']."</option>";
+                    }
+                    echo "</select><br/>";
+                    echo "<input class=\"btn btn-warning\" type=\"submit\" value=\"Supprimer l'(les) offre(s)\"/>";
+                    echo "</form>";
 
                 }
             ?>
--- function.php
+++ function.php
@@ -1,18 +1,38 @@
 <?php
 require('Config.php');
 session_start();
+
+
 if(isset($_POST['mail']))
 {
     Connection($_POST['mail'],$_POST['password'],$bdd);
 }
-
-if($_GET['deconnect'])
+if(isset($_GET['deconnect']))
 {
     session_destroy();
     header("Location: index.php");
 }
+if(isset($_POST['deleteUser']))
+{
+    var_dump($_POST);
+    DeleteUser($_POST['user'], $bdd);
+}
+if(isset($_POST['deleteRequest']))
+{
+    DeleteRequest($_POST['request'], $bdd);
+}
+if(isset($_POST['deleteOffer']))
+{
+    DeleteRequest($_POST['offer'], $bdd);
+}
 
-function Connection($mail, $password,$bdd)
+
+/**
+ * @param $mail
+ * @param $password
+ * @param $bdd
+ */
+function Connection($mail, $password, $bdd)
 {
     $request = "SELECT * FROM User where mail = \"$mail\"";
     $respond = $bdd->query($request);
@@ -33,11 +53,54 @@ function Connection($mail, $password,$bdd)
         else
         {
             $connected = 2;
-            echo "Erreur de connexion";
+            echo "<script language='JavaScript'>alert(\"Erreur de connexion\")</script>";
+            sleep(2);
+            header("Location: index.php");
         }
     }
     if ($connected==1)
     {
-        echo "Le compte n'existe pas";
+        echo "<script language='JavaScript'>alert(\"Le compte n'existe pas\")</script>";
+        sleep(2);
+        header("Location: index.php");
     }
+}
+
+/**
+ * @param $id
+ * @param $bdd
+ */
+function DeleteUser($id, $bdd)
+{
+    $request = "DELETE FROM User WHERE Id= $id";
+    echo $request;
+    $req = $bdd->prepare($request);
+    $req->execute();
+    header("Location: admin.php");
+}
+
+/**
+ * @param $id
+ * @param $bdd
+ */
+function DeleteRequest($id, $bdd)
+{
+    $request = "DELETE FROM Requests WHERE Id= $id";
+    echo $request;
+    $req = $bdd->prepare($request);
+    $req->execute();
+    header("Location: admin.php");
+}
+
+/**
+ * @param $id
+ * @param $bdd
+ */
+function DeleteOffer($id, $bdd)
+{
+    $request = "DELETE FROM Offers WHERE Id= $id";
+    echo $request;
+    $req = $bdd->prepare($request);
+    $req->execute();
+    header("Location: admin.php");
 }
