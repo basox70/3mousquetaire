a970e7b960caaa0dac4bbb001d24cc134796372e
BasileDubruque <basox70@users.noreply.github.com>
2016-10-06 10:44:20 +0200

    Update requests.php

--- requests.php
+++ requests.php
@@ -4,9 +4,10 @@ include('Config.php');
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Yardim - Accueil</title>
-    <link href="css/style.css" rel="stylesheet">
+    <title>Yardim - Demande</title>
     <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 </head>
 <body>
 <div class="page-header">
@@ -64,18 +65,10 @@ include('Config.php');
             echo '<div class="col-sm-6">'.$donnees['Nickname'].'</div>';
         echo '</div>';
 
-
         echo '<button class="btn btn-success">J\'envoie</button>';
         echo '<button class="btn btn-warning"><a href="index.php">Page Précedente</a></button>';
     }
-
-
-
-
-
-
     echo '</div>';
-
     ?>
 </div>
 </body>
